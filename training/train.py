abf5998f2385bf22a77d1fa876490c1b24e2e711
Minjun Jang <jun1557@snu.ac.kr>
2024-05-27 15:08:43 +0900

    ToTensor 제거

--- training/train.py
+++ training/train.py
@@ -55,7 +55,6 @@ def main(args):
 
     transform = Compose([
         Resize((224, 224)),
-        ToTensor(),
         FrameNormalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
     ])
 
