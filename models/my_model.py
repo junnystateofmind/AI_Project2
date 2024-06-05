67c3dfb086494fa804581f32ce748ac5f0ac0fb5
Minjun Jang <jun1557@snu.ac.kr>
2024-06-05 21:46:35 +0900

    EfficientNet Lite로 변경

--- models/my_model.py
+++ models/my_model.py
@@ -1,20 +1,13 @@
 import torch
 import torch.nn as nn
-from torchvision import models
+from efficientnet_pytorch import EfficientNet
 
 class MyModel(nn.Module):
     def __init__(self, num_classes=101):
         super(MyModel, self).__init__()
-        # MobileNetV2 모델 불러오기
-        self.mobilenet = models.mobilenet_v2(pretrained=True)
-        self.mobilenet.classifier = nn.Identity()  # 마지막 분류기 레이어 제거
-
-        # MobileNetV2의 첫 번째 레이어를 수정하여 240 채널을 처리하도록 변경
-        self.mobilenet.features[0][0] = nn.Conv2d(240, 32, kernel_size=3, stride=2, padding=1, bias=False)
-
-        # MobileNetV2 레이어를 동결
-        for param in self.mobilenet.parameters():
-            param.requires_grad = False
+        # EfficientNet-Lite 모델 불러오기
+        self.efficientnet = EfficientNet.from_pretrained('efficientnet-lite0')
+        self.efficientnet._fc = nn.Identity()  # 마지막 분류기 레이어 제거
 
         # LSTM 및 Fully Connected 레이어 정의
         self.lstm = nn.LSTM(1280, 128, batch_first=True)
@@ -22,35 +15,16 @@ class MyModel(nn.Module):
 
     def forward(self, x):
         batch_size, num_frames, channels, height, width = x.size()
-
-        # (batch_size, num_frames, channels, height, width) -> (batch_size * num_frames, channels, height, width)
         x = x.view(-1, channels, height, width)
-
-        # CNN 통과
-        cnn_features = self.mobilenet(x)  # (batch_size * num_frames, 1280)
-
-        # (batch_size, num_frames, 1280)로 변환
+        cnn_features = self.efficientnet(x)  # (batch_size * num_frames, 1280)
         cnn_features = cnn_features.view(batch_size, num_frames, -1)
-
-        # LSTM 통과
         lstm_output, _ = self.lstm(cnn_features)  # (batch_size, num_frames, 128)
-
-        # Global Average Pooling 적용
         lstm_output = lstm_output.mean(dim=1)  # (batch_size, 128)
-
-        # Fully Connected Layer 통과
         output = self.fc(lstm_output)  # (batch_size, num_classes)
-
         return output
 
-from torchinfo import summary
-
-# 모델 인스턴스 생성
+# 모델 인스턴스 생성 및 요약 정보 출력
 model = MyModel(num_classes=101)
-
-# 모델을 GPU로 이동
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 model.to(device)
-
-# 모델 요약 정보 출력
-summary(model, input_size=(8, 10, 240, 224, 224), device=device.type)  # (batch_size, num_frames, channels, height, width)
+summary(model, input_size=(8, 10, 3, 224, 224), device=device.type)  # (batch_size, num_frames, channels, height, width)
