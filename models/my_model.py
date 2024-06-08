dc5250b69df2d08cb2894cf203425bcfc7dedd54
Minjun Jang <jun1557@snu.ac.kr>
2024-06-08 13:59:06 +0900

    101개로 변경

--- models/my_model.py
+++ models/my_model.py
@@ -16,15 +16,13 @@ class MyModel(nn.Module):
         for param in self.efficientnet.parameters():
             param.requires_grad = False
 
-        # 1x1 컨볼루션 레이어 추가 (차원 축소용)
-        connected_dimension = 320
-        self.conv1x1 = nn.Conv2d(1280, 640, kernel_size=1, stride=1, padding=0, bias=False)
-        self.bn = nn.BatchNorm2d(connected_dimension)
-        self.relu = nn.ReLU()
+        # Global Average Pooling 및 Fully Connected 레이어 정의
+        self.gap = nn.AdaptiveAvgPool2d((1, 1))
+        self.fc1 = nn.Linear(1280, 101)
 
-        # LSTM 및 Fully Connected 레이어 정의
-        self.lstm = nn.LSTM(input_size=connected_dimension, hidden_size=128, num_layers=1, batch_first=True)
-        self.fc = nn.Linear(128, num_classes)
+        # LSTM 및 최종 Fully Connected 레이어 정의
+        self.lstm = nn.LSTM(101, 128, batch_first=True)
+        self.fc2 = nn.Linear(128, num_classes)
 
     def forward(self, x):
         batch_size, num_frames, channels, height, width = x.size()
@@ -35,13 +33,14 @@ class MyModel(nn.Module):
         # CNN 통과
         cnn_features = self.efficientnet(x)  # (batch_size * num_frames, 1280, 7, 7)
 
-        # 차원 축소 (1x1 컨볼루션 레이어 통과)
-        cnn_features = self.relu(self.bn(self.conv1x1(cnn_features)))  # (batch_size * num_frames, 640, 7, 7)
+        # Global Average Pooling 적용
+        cnn_features = self.gap(cnn_features)  # (batch_size * num_frames, 1280, 1, 1)
+        cnn_features = cnn_features.view(cnn_features.size(0), -1)  # (batch_size * num_frames, 1280)
 
-        # Global Average Pooling
-        cnn_features = torch.mean(cnn_features, dim=[2, 3])  # (batch_size * num_frames, 640)
+        # Fully Connected Layer 통과
+        cnn_features = self.fc1(cnn_features)  # (batch_size * num_frames, 101)
 
-        # (batch_size, num_frames, 640)로 변환
+        # (batch_size, num_frames, 101)로 변환
         cnn_features = cnn_features.view(batch_size, num_frames, -1)
 
         # LSTM 통과
@@ -50,7 +49,7 @@ class MyModel(nn.Module):
         # LSTM 출력에 대해 Global Average Pooling 적용
         lstm_output = lstm_output.mean(dim=1)  # (batch_size, 128)
 
-        # Fully Connected Layer 통과
-        output = self.fc(lstm_output)  # (batch_size, num_classes)
+        # 최종 Fully Connected Layer 통과
+        output = self.fc2(lstm_output)  # (batch_size, num_classes)
 
         return output
