0dc7186b52fd2a553ecb51fcf8eea675b4e0932a
Minjun Jang <jun1557@snu.ac.kr>
2024-06-10 23:22:44 +0900

    custom_collate_fn 함수에서 디버깅 정보를 출력

--- training/train.py
+++ training/train.py
@@ -5,14 +5,94 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 import torch
 import torch.optim as optim
-from torch.utils.data import DataLoader, random_split
-from torchvision.datasets import UCF101
+from torch.utils.data import DataLoader, random_split, Dataset
 from argparse import ArgumentParser
-from torchvision.transforms import Compose, Resize, Normalize
+from torchvision.transforms import Compose, Resize, Normalize, ToPILImage
 from models.my_model import MyModel
 from tqdm import tqdm
 from torchinfo import summary
 from torch.cuda.amp import autocast, GradScaler
+import random
+import pandas as pd
+import numpy as np
+import skvideo.io
+
+
+class UCF101Dataset(Dataset):
+    def __init__(self, root_dir, clip_len, split='1', train=True, transforms_=None, test_sample_num=10):
+        self.root_dir = root_dir
+        self.clip_len = clip_len
+        self.split = split
+        self.train = train
+        self.transforms_ = transforms_
+        self.test_sample_num = test_sample_num
+        self.toPIL = ToPILImage()
+        class_idx_path = os.path.join(root_dir, 'split', 'classInd.txt')
+        self.class_idx2label = pd.read_csv(class_idx_path, header=None, sep=' ').set_index(0)[1]
+        self.class_label2idx = pd.read_csv(class_idx_path, header=None, sep=' ').set_index(1)[0]
+
+        if self.train:
+            train_split_path = os.path.join(root_dir, 'split', 'trainlist0' + self.split + '.txt')
+            self.train_split = pd.read_csv(train_split_path, header=None, sep=' ')[0]
+        else:
+            test_split_path = os.path.join(root_dir, 'split', 'testlist0' + self.split + '.txt')
+            self.test_split = pd.read_csv(test_split_path, header=None)[0]
+        print('Use split' + self.split)
+
+    def __len__(self):
+        if self.train:
+            return len(self.train_split)
+        else:
+            return len(self.test_split)
+
+    def __getitem__(self, idx):
+        if self.train:
+            videoname = self.train_split[idx]
+        else:
+            videoname = self.test_split[idx]
+        class_idx = self.class_label2idx[videoname[:videoname.find('/')]]
+        filename = os.path.join(self.root_dir, 'video', videoname)
+        videodata = skvideo.io.vread(filename)
+        length, height, width, channel = videodata.shape
+
+        if self.train:
+            clip_start = random.randint(0, length - self.clip_len)
+            clip = videodata[clip_start: clip_start + self.clip_len]
+
+            if self.transforms_:
+                trans_clip = []
+                seed = random.random()
+                for frame in clip:
+                    random.seed(seed)
+                    frame = self.toPIL(frame)
+                    frame = self.transforms_(frame)
+                    trans_clip.append(frame)
+                clip = torch.stack(trans_clip).permute([1, 0, 2, 3])
+            else:
+                clip = torch.tensor(clip)
+
+            return clip, int(class_idx)
+        else:
+            all_clips = []
+            all_idx = []
+            for i in np.linspace(self.clip_len / 2, length - self.clip_len / 2, self.test_sample_num):
+                clip_start = int(i - self.clip_len / 2)
+                clip = videodata[clip_start: clip_start + self.clip_len]
+                if self.transforms_:
+                    trans_clip = []
+                    seed = random.random()
+                    for frame in clip:
+                        random.seed(seed)
+                        frame = self.toPIL(frame)
+                        frame = self.transforms_(frame)
+                        trans_clip.append(frame)
+                    clip = torch.stack(trans_clip).permute([1, 0, 2, 3])
+                else:
+                    clip = torch.tensor(clip)
+                all_clips.append(clip)
+                all_idx.append(int(class_idx))
+
+            return torch.stack(all_clips), torch.tensor(all_idx)
 
 
 class FrameNormalize:
@@ -32,10 +112,16 @@ def custom_collate_fn(batch):
     videos = [item[0] for item in batch]
     labels = [item[1] for item in batch]
 
-    # Ensure all labels are tensors of the same size
-    # TypeError: only integer tensors of a single element can be converted to an index 발생, 확인하기 위해서 타입 출력
-    print(f"labels type: {type(labels)}")
-    labels = torch.tensor(labels, dtype=torch.int64)
+    # Debugging: Print the types and lengths of labels
+    print(f"Labels: {labels}")
+    print(f"Labels type: {[type(label) for label in labels]}")
+    print(f"Labels length: {len(labels)}")
+
+    try:
+        labels = torch.tensor(labels, dtype=torch.int64)
+    except Exception as e:
+        print(f"Error converting labels to tensor: {e}")
+        raise e
 
     videos = torch.stack(videos)
     return videos, labels
@@ -88,8 +174,7 @@ def main(args):
     ])
 
     print("Loading datasets...")
-    full_dataset = UCF101(root='./data/UCF-101', annotation_path='./data/annotations/ucfTrainTestlist',
-                          frames_per_clip=16, step_between_clips=1, fold=1, train=True, transform=transform)
+    full_dataset = UCF101Dataset(root='./data/UCF-101', clip_len=16, split='1', train=True, transforms_=transform)
     train_size = int(0.8 * len(full_dataset))
     test_size = len(full_dataset) - train_size
     train_dataset, test_dataset = random_split(full_dataset, [train_size, test_size])
